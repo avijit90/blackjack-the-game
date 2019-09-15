ad19b8d1f7aa3fdf4be849f64f4cd4b62af01a03
Avijit Vaidya <avijit.vaidya@finastra.com>
2019-09-15 16:06:47 +0800

    __str__ method for card class.

--- src/game/model/Card.py
+++ src/game/model/Card.py
@@ -5,3 +5,7 @@ class Card:
         self.face_img = face_img
         self.back_img = back_img
         self.visible = False
+
+    def __str__(self):
+        return f'face_value : {self.face_value},\n face_img : {self.face_img},\n ' \
+               f'back_img : {self.back_img},\n visible : {self.visible}'
