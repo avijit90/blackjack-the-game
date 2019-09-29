1c2cd212305744a08958f1d7f47d8322095aef0a
avijit90 <sepiaavi@gmail.com>
2019-09-29 22:27:38 +0800

    Added money to replicate bets.

--- src/game/model/Player.py
+++ src/game/model/Player.py
@@ -4,6 +4,7 @@ class Player:
         self.name = name
         self.score = 0
         self.current_cards = []
+        self.money = 100
 
     def __str__(self):
         return f'name : {self.name}, score : {self.score}, ' \
@@ -12,3 +13,7 @@ class Player:
     def collect_card(self, new_card):
         self.current_cards.append(new_card)
         self.score += new_card.face_value
+
+    def prepare_for_new_round(self):
+        self.score = 0
+        self.current_cards = []
